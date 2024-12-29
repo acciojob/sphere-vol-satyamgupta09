6b1ba72833677a0a4c3df1a9ef2511c7aace9081
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-30 00:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
-    //Write your code here
-  
+	event.preventDefault();
+	let r = parseFloat(document.getElementById('radius').value);
+	let v = document.getElementById('volume');
+	
+    if(isNaN(r) || r<0){
+		v.value = "NaN";
+		return;
+	}
+
+	let volume = (4/3)*3.14*r*r*r;
+
+	v.value = volume.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
